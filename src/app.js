1ed4d1432470a96d12ebc40bfeaff54104d4b31c
Andrew Stoker <TheAndyBob@me.com>
2016-06-17 15:53:48 -0400

    Initial Checkin
    Issues with binding on child routes (must navigate 2 deep and refresh
    to experience issue)
    https://github.com/code-chris/aurelia-knockout/issues/5

---
+++ src/app.js
@@ -0,0 +1,18 @@
+export class App {
+    constructor(){
+        this.foo = 'bar';
+    }
+    activate(){
+        console.log('activated app');
+    }
+  configureRouter(config, router) {
+    config.title = 'Aurelia';
+    config.map([
+      { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' },
+      { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' },
+      { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' }
+    ]);
+
+    this.router = router;
+  }
+}
